750a7f057ec0223c1474d2902bdc724926964cfc
Vamsi Charan Adari <adarivamsi@gmail.com>
2021-11-11 17:58:12 -0600

    Added

--- Arrays/containerWithMostWater/script.js
+++ Arrays/containerWithMostWater/script.js
@@ -1,38 +1,25 @@
 console.clear();
-console.log("Three Sum");
+console.log("Container With Most Water");
 
-var threeSum = function (nums) {
-  let res = [];
-  nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
-    let l = i + 1;
-    let r = nums.length - 1;
-    console.log(`i: ${i}, l: ${l}, r: ${r}, res: ${res}`);
-    while (l < r) {
-      let sum = nums[i] + nums[l] + nums[r];
-      console.log(`i: ${i}, l: ${l}, r: ${r}, res: ${res}, sum: ${sum}`);
-      if (sum > 0) {
-        r -= 1;
-      } else if (sum < 0) {
-        l += 1;
-      } else {
-        res.push([nums[i], nums[l], nums[r]]);
-        l += 1;
-        while (nums[l] === nums[l - 1] && l < r) {
-          l += 1;
-          console.log(
-            `Added - i: ${i}, l: ${l}, r: ${r}, res: ${res}, sum: ${sum}`
-          );
-        }
-      }
+var maxArea = function (height) {
+  let l = 0;
+  let r = height.length - 1;
+  let res = 0;
+  while (l < r) {
+    let area = (r - l) * Math.min(height[l], height[r]);
+    res = Math.max(res, area);
+    console.log(`Left - l: ${l}, r: ${r}, area: ${area}, res: ${res}`);
+
+    if (height[l] < height[r]) {
+      l += 1;
+      console.log(`Left - l: ${l}, r: ${r}, area: ${area}, res: ${res}`);
+    } else {
+      r -= 1;
+      console.log(`Right - l: ${l}, r: ${r}, area: ${area}, res: ${res}`);
     }
   }
 
   return res;
 };
 
-// console.log(threeSum([-1, 0, 1, 2, -1, -4]));
-// console.log(threeSum([]));
-// console.log(threeSum([0]));
-console.log(threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]));
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
