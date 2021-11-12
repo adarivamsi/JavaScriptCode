a8b1232e6f8bf9db355df1bc5399b4b7fe75d3cd
Vamsi Charan Adari <adarivamsi@gmail.com>
2021-11-11 18:55:19 -0600

    Added

---
+++ Binary/countingBits/script.js
@@ -0,0 +1,14 @@
+console.clear();
+console.log("Counting Bits");
+
+var countBits = function (n) {
+  let dp = new Array(n + 1).fill(0);
+  let offset = 1;
+  for (let i = 1; i < n + 1; i++) {
+    if (offset * 2 == i) offset = i;
+    dp[i] = 1 + dp[i - offset];
+  }
+  return dp;
+};
+
+console.log(countBits(2));
